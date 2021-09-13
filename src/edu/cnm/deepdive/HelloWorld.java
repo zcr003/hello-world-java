e5b365c5319498c20eb427f017cc58e145ee95c3
zcr003 <your@email.com>
2021-09-12 21:31:31 -0600

    Decomposed main with target in cmd line.

--- src/edu/cnm/deepdive/HelloWorld.java
+++ src/edu/cnm/deepdive/HelloWorld.java
@@ -3,7 +3,14 @@ package edu.cnm.deepdive;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    greet("Hello, " + target(args) + "!");
   }
 
+  private static String target(String[] args) {
+    return (args.length > 0) ? args[0] : "world";
+
+  }
+ private static void greet(String greeting) {
+    System.out.println(greeting);
+ }
 }
